72334107581c32900054ce5d2294209213612b4b
Balaji Leninrajan <bleninrajan@uwaterloo.ca>
2024-07-11 08:00:03 +0530

    Added Material You support

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -29,64 +30,75 @@ class MyApp extends StatelessWidget {
     return ListenableBuilder(
       listenable: settingsController,
       builder: (BuildContext context, Widget? child) {
-        return MaterialApp(
-          // Providing a restorationScopeId allows the Navigator built by the
-          // MaterialApp to restore the navigation stack when a user leaves and
-          // returns to the app after it has been killed while running in the
-          // background.
-          restorationScopeId: 'app',
+        return DynamicColorBuilder(
+          builder:
+              (ColorScheme? lightColorScheme, ColorScheme? darkColorScheme) {
+            return MaterialApp(
+              // Providing a restorationScopeId allows the Navigator built by the
+              // MaterialApp to restore the navigation stack when a user leaves and
+              // returns to the app after it has been killed while running in the
+              // background.
+              restorationScopeId: 'app',
 
-          // Provide the generated AppLocalizations to the MaterialApp. This
-          // allows descendant Widgets to display the correct translations
-          // depending on the user's locale.
-          localizationsDelegates: const [
-            AppLocalizations.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-          ],
-          supportedLocales: const [
-            Locale('en', ''), // English, no country code
-          ],
+              // Provide the generated AppLocalizations to the MaterialApp. This
+              // allows descendant Widgets to display the correct translations
+              // depending on the user's locale.
+              localizationsDelegates: const [
+                AppLocalizations.delegate,
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+              ],
+              supportedLocales: const [
+                Locale('en', ''), // English, no country code
+              ],
 
-          // Use AppLocalizations to configure the correct application title
-          // depending on the user's locale.
-          //
-          // The appTitle is defined in .arb files found in the localization
-          // directory.
-          onGenerateTitle: (BuildContext context) =>
-              AppLocalizations.of(context)!.appTitle,
+              // Use AppLocalizations to configure the correct application title
+              // depending on the user's locale.
+              //
+              // The appTitle is defined in .arb files found in the localization
+              // directory.
+              onGenerateTitle: (BuildContext context) =>
+                  AppLocalizations.of(context)!.appTitle,
 
-          // Define a light and dark color theme. Then, read the user's
-          // preferred ThemeMode (light, dark, or system default) from the
-          // SettingsController to display the correct theme.
-          theme: ThemeData(),
-          darkTheme: ThemeData.dark(),
-          themeMode: settingsController.themeMode,
+              // Define a light and dark color theme. Then, read the user's
+              // preferred ThemeMode (light, dark, or system default) from the
+              // SettingsController to display the correct theme.
+              theme: ThemeData(
+                colorScheme: lightColorScheme,
+                useMaterial3: true,
+              ),
+              darkTheme: ThemeData(
+                colorScheme: darkColorScheme ?? const ColorScheme.dark(),
+                useMaterial3: true,
+              ),
+              themeMode: settingsController.themeMode,
 
-          // Define a function to handle named routes in order to support
-          // Flutter web url navigation and deep linking.
-          onGenerateRoute: (RouteSettings routeSettings) {
-            return MaterialPageRoute<void>(
-              settings: routeSettings,
-              builder: (BuildContext context) {
-                if (settingsController.loginFlag.isEmpty) {
-                  return LoginScreen(controller: settingsController);
-                }
-                switch (routeSettings.name) {
-                  case SettingsScreen.routeName:
-                    return SettingsScreen(controller: settingsController);
-                  case AddClaimScreen.routeName:
-                    return AddClaimScreen(controller: claimController);
-                  case LoginScreen.routeName:
-                    return LoginScreen(controller: settingsController);
-                  case HomeScreen.routeName:
-                  default:
-                    return HomeScreen(
-                      claimController: claimController,
-                      settingsController: settingsController,
-                    );
-                }
+              // Define a function to handle named routes in order to support
+              // Flutter web url navigation and deep linking.
+              onGenerateRoute: (RouteSettings routeSettings) {
+                return MaterialPageRoute<void>(
+                  settings: routeSettings,
+                  builder: (BuildContext context) {
+                    if (settingsController.loginFlag.isEmpty) {
+                      return LoginScreen(controller: settingsController);
+                    }
+                    switch (routeSettings.name) {
+                      case SettingsScreen.routeName:
+                        return SettingsScreen(controller: settingsController);
+                      case AddClaimScreen.routeName:
+                        return AddClaimScreen(controller: claimController);
+                      case LoginScreen.routeName:
+                        return LoginScreen(controller: settingsController);
+                      case HomeScreen.routeName:
+                      default:
+                        return HomeScreen(
+                          claimController: claimController,
+                          settingsController: settingsController,
+                        );
+                    }
+                  },
+                );
               },
             );
           },
